45ef301450e845cb0c0fa1f370735c56d1d16493
shish-ko <andrey.shishko@gmail.com>
2023-09-20 16:42:08 +0300

    fix: horizontal chipScroller

--- src/components/WelcomePage/WelcomeAnimationHoriz.tsx
+++ src/components/WelcomePage/WelcomeAnimationHoriz.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import horiz_1 from '../../assets/images/animation_chips/Line_Vertical_2.png';
+import horiz_1 from '../../assets/images/animation_chips/Line_Vertical_1.png';
+import horiz_2 from '../../assets/images/animation_chips/Line_Vertical_2.png';
+import horiz_3 from '../../assets/images/animation_chips/Line_Vertical_3.png';
 
 export const WelcomeAnimationHoriz: React.FC = () => {
   return (
@@ -9,6 +11,18 @@ export const WelcomeAnimationHoriz: React.FC = () => {
         <img src={horiz_1}></img>
         <img src={horiz_1}></img>
         <img src={horiz_1}></img>
+      </div>
+      <div className="chipScrollHorizontal__line chipScrollHorizontal__line_right">
+        <img src={horiz_2}></img>
+        <img src={horiz_2}></img>
+        <img src={horiz_2}></img>
+        <img src={horiz_2}></img>
+      </div>
+      <div className="chipScrollHorizontal__line chipScrollHorizontal__line_left">
+        <img src={horiz_3}></img>
+        <img src={horiz_3}></img>
+        <img src={horiz_3}></img>
+        <img src={horiz_3}></img>
       </div>
     </div>
   )
