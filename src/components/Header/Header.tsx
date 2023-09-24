0aac88a9bb2ba422a2d2de465e9b0b352a7590f0
shish-ko <shishko.dev@gamil.com>
2023-09-25 00:10:13 +0300

    feat: burger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import { BurgerIcon } from "@comps/UI_comps/BurgerIcon";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import imgUrl from '../../assets/Icon-Planet.png'
+import imgUrl from '../../assets/Icon-Planet.svg'
 
 export const Header: React.FC = () => {
   const [english, setEnglish] = useState(true);
@@ -12,7 +12,7 @@ export const Header: React.FC = () => {
   return (
         <header className="header">
           <div className="header__item"/>
-          <BurgerIcon menuHandler={menuHandler}/>
+          <BurgerIcon menuHandler={menuHandler} className='header__left-burger'/>
           <nav className={`menu ${isMenuOpen && 'menu_open'}`}>
             <ul className="menu__list">
               <li className="menu__item menu-link"><Link to={'/about'}>About us</Link></li>
@@ -25,11 +25,12 @@ export const Header: React.FC = () => {
           </nav>
           <div className="header__auth">
             <button className="menu-btn outlined withPic" onClick={() => setEnglish(!english)}>
-              <img src={imgUrl}></img>
-              <span>{english ? 'en' : 'ru'}</span>
+              <img src={imgUrl} alt='current language' className="header__lang-pic"></img>
+              <span className="header__current-lang">{english ? 'en' : 'ru'}</span>
               </button>
-            <button className="menu-btn outlined">log in</button>
-            <button className="menu-btn filled-item">sign up</button>
+            <button className="menu-btn outlined header__logIn">log in</button>
+            <button className="menu-btn filled-item header__signUp">sign up</button>
+            <BurgerIcon menuHandler={menuHandler} className='header__right-burger'/>
           </div>
         </header>
   )
--- src/components/UI_comps/BurgerIcon.tsx
+++ src/components/UI_comps/BurgerIcon.tsx
@@ -1,12 +1,13 @@
-import { useState } from "react"
+import React, { useState } from "react"
 interface IBurgerIcon {
   menuHandler: () => void;
+  className?: string;
 }
-export const BurgerIcon: React.FC<IBurgerIcon> = ({menuHandler}) => {
+export const BurgerIcon: React.FC<IBurgerIcon> = ({menuHandler, className}) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="burgerIcon" onClick={() => {setIsOpen(!isOpen); menuHandler()}}>
+    <div className={`burgerIcon ${className}`} onClick={() => {setIsOpen(!isOpen); menuHandler()}}>
       <div className={`burgerIcon__line burgerIcon__lineTop ${isOpen && 'burgerIcon__lineTop_open'}`}></div>
       <div className={`burgerIcon__line ${isOpen && 'burgerIcon__line_center'}`}></div>
       <div className={`burgerIcon__line burgerIcon__lineBottom ${isOpen && 'burgerIcon__lineBottom_open'}`}></div>
